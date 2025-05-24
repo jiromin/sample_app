01b8b1cfd46e7335c43c73afdfac5890beabd89e
jiromin <96801969+jiromin@users.noreply.github.com>
2025-05-24 01:12:07 +0000

    Finish user edit, update, index, and destroy actions

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,9 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+require 'will_paginate'
+require 'will_paginate/active_record'
+
 module SampleApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
